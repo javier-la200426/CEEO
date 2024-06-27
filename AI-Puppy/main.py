90fa312c33261c139fcb8a42c0ff37814744c4c1
javier-la200426 <157245009+javier-la200426@users.noreply.github.com>
2024-06-27 10:38:43 -0400

    Create main.py

---
+++ AI-Puppy/main.py
@@ -0,0 +1,29 @@
+# main.py -- put your code here!
+import hub, utime, color
+import force_sensor as fs
+import distance_sensor as ds
+import color_sensor as cs
+from hub import port, button, light_matrix, sound, light
+import time
+import device
+'''
+Iterate through all ports,
+
+    Get the ID from individual port
+
+        Map it to a value
+        (keys are ID's and values are function pointers)
+        Call corresponding function
+'''
+
+
+
+function_dict = {
+    49:
+    61:
+    62:
+    63:
+    64:
+    65: 
+}
+
